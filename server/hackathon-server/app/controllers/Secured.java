e81716ada94b80630e9ab6eca1f19eeb8d846f23
Glenn Sayers <glennsayers.gs@gmail.com>
2014-09-13 17:12:35 +0100

    Added basic auth

---
+++ server/hackathon-server/app/controllers/Secured.java
@@ -0,0 +1,31 @@
+package controllers;
+
+import play.mvc.Http;
+import play.mvc.Result;
+import play.mvc.Security;
+
+/**
+ * Created by gsayers on 13/09/2014.
+ */
+public class Secured extends Security.Authenticator {
+
+    /**
+     * Fetches the users email from the current session.
+     * @param ctx Application context
+     * @return The current username
+     */
+    @Override
+    public final String getUsername(final Http.Context ctx) {
+        return ctx.session().get("id");
+    }
+
+    /**
+     * If the user is unauthorised, redirect back to the login page.
+     * @param ctx Application context
+     * @return Result()
+     */
+    @Override
+    public final Result onUnauthorized(final Http.Context ctx) {
+        return redirect(controllers.routes.Application.());
+    }
+}
