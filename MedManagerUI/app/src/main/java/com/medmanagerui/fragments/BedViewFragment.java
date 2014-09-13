11a91af2e3b6b1f1915a5dcaa638ba0134effa80
Rory Powell <rpowell193@gmail.com>
2014-09-13 17:54:56 +0100

    Refactor

--- MedManagerUI/app/src/main/java/com/medmanagerui/fragments/BedViewFragment.java
+++ MedManagerUI/app/src/main/java/com/medmanagerui/fragments/BedViewFragment.java
@@ -1,6 +1,5 @@
-package com.medmanagerui;
+package com.medmanagerui.fragments;
 
-import android.app.Activity;
 import android.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
@@ -11,6 +10,8 @@ import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.Toast;
 
+import com.medmanagerui.R;
+import com.medmanagerui.adapters.GridViewAdapter;
 
 
 public class BedViewFragment extends Fragment {
