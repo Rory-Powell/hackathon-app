49f48dd63d146e2b3f4ec80920da65f79c718ac4
Darren Murray <dmurray@BFS-MAC-17.local>
2014-09-13 18:49:55 +0100

    base for Dialog for patient check in

---
+++ MedManagerUI/app/src/main/java/com/medmanagerui/fragments/PatientCheckInDialogFragment.java
@@ -0,0 +1,34 @@
+package com.medmanagerui.fragments;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import com.medmanagerui.R;
+
+/**
+ * Created by dmurray on 13/09/2014.
+ */
+public class PatientCheckInDialogFragment extends DialogFragment{
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the Builder class for convenient dialog construction
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setMessage(R.string.title_checkinDialog)
+                    .setPositiveButton(R.string.select_PatientDialog, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // FIRE ZE MISSILES!
+                        }
+                    })
+                    .setNegativeButton(R.string.select_DoctorDialog, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // User cancelled the dialog
+                        }
+                    });
+            // Create the AlertDialog object and return it
+            return builder.create();
+        }
+    }
+
